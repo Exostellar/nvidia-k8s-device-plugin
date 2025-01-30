18d4908a7711d8556810fa03c25382b19b2d828c
hfingler <henrique@exostellar.io>
2025-01-30 04:40:45 +0000

    omni fixes

--- gcp_build.sh
+++ gcp_build.sh
@@ -8,5 +8,7 @@ docker build \
     -f deployments/container/Dockerfile.ubuntu \
     .
 
+# gcloud auth configure-docker us-central1-docker.pkg.dev
+
 docker tag nvcr.io/nvidia/k8s-device-plugin:devel us-central1-docker.pkg.dev/poc-omniops-gpu/sdg/nvidia-dp:latest
 docker push us-central1-docker.pkg.dev/poc-omniops-gpu/sdg/nvidia-dp:latest
--- gcp_update_helm_install.sh
+++ gcp_update_helm_install.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 #nvidia-device-plugin
 
-helm upgrade --install --wait \
-    -n kube-system \
-    nvdp \
-    ./deployments/helm/nvidia-device-plugin \
-    --set hostPaths.driverInstallDir=/home/kubernetes/bin/nvidia \
-    --set toolkit.installDir=/home/kubernetes/bin/nvidia \
-    --set cdi.enabled=true \
-    --set cdi.default=true \
-    --set driver.enabled=false
+# gcloud auth configure-docker us-central1-docker.pkg.dev
 
 #helm upgrade --install nvdp ./deployments/helm/nvidia-device-plugin \
 #     --namespace kube-system \
 #  -f deployments/helm/nvidia-device-plugin/values_gcp.yaml
 
-#kubectl rollout restart daemonset nvdp-nvidia-device-plugin -n kube-system
+kubectl rollout restart daemonset nvdp-nvidia-device-plugin -n kube-system
 
